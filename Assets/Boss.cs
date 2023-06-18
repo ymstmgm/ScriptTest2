83c1a498e21afa07b50680b95f64bfabd9a61ed8
ymstmgm <134988637+ymstmgm@users.noreply.github.com>
2023-06-18 13:28:36 +0900

    Initial commit

---
+++ Assets/Boss.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    int mp = 53;
+    int magic = 53;
+
+
+
+    public void Attack()
+    {
+        if (magic >= 5)
+        {
+            // mpが5以上だった場合5減らす
+            mp -= 5;
+            {
+              Debug.Log("魔法攻撃をした。残り○○。");
+            }
+
+        }
+
+        else
+        {
+            // mpが足りない時
+            {
+              Debug.Log("mpが足りない。");
+            }
+
+        }
+
+        // Start is called before the first frame update
+        
+        {
+            Boss lastboss = new Boss();
+
+           {
+              // 10回繰り返す
+              for (int mp = 0; mp < 5; mp++) ;
+           }
+            {
+              Debug.Log("mpが足りない。");
+
+            }
+        }
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] array = new int[5];
+
+        // 配列の各要素に値を代入する
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        // 配列の要素をすべて表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        // 配列の要素を逆順表示する
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
